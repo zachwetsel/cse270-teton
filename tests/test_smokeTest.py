fde93556455cb234cd70a3599483bc40ee7d8d14
zachwetsel <zachwetsel@yahoo.com>
2025-02-19 21:45:30 -0700

    Commit Headless Smoke

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,10 +9,13 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
